7516a2f9a192e28e948116cd2b4d7ff6c25f6c91
Raphael Bialon <bialon@cs.uni-duesseldorf.de>
2019-03-08 20:35:02 +0100

    Add location Views and templates for AJAX requests

--- locations/urls.py
+++ locations/urls.py
@@ -6,6 +6,7 @@ app_name = 'locations'
 
 urlpatterns = [
     path('', views.IndexLocation.as_view(), name='index'),
+    path('as-options/', views.IndexOptionsLocation.as_view(), name='as-options'),
     path('detail/<int:location_id>/', views.DetailLocation.as_view(), name='detail'),
     path('create/', views.CreateLocation.as_view(), name='create'),
     path('edit/<int:location_id>', views.EditLocation.as_view(), name='edit'),

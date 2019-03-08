6d6f531287c21a63b5a19a0465b05216a303b40e
Alexander Schneider <alexander@schneider.gg>
2019-03-08 11:16:09 +0100

    Change PlaylistItem to be used as a ForeignKey

--- scenes/models.py
+++ scenes/models.py
@@ -4,12 +4,16 @@ from user_manager.models import User
 
 
 class PlaylistItem(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
     name = models.CharField("Name", max_length=100, null=False, default="Ambience Music")
     # To which service does the playlist belong? (Default is Spotify, but can be more in future)
     service = models.CharField("Service", max_length=100, null=False, default="Spotify")
     # We do not use Djangos URL field because in the future we might need more complex content
     uri = models.CharField("URI", max_length=1000, null=False,
                            default="https://open.spotify.com/user/cate.falconer/playlist/7dFPewHpqOdEb3E88AZhYC?si=sFYcXn4SRSOF2sHYoFQ1Yg")
+
+    def __str__(self):
+        return self.name
 
 
 # TODO This model will change in the future. All fields, especially place, characters, rewards and encounters
@@ -22,7 +26,7 @@ class Scene(models.Model):
     characters = models.TextField("Characters", max_length=20000, null=True, blank=True)
     rewards = models.TextField("Rewards", max_length=20000, null=True, blank=True)
     encounters = models.TextField("Encounters", max_length=20000, null=True, blank=True)
-    sound_effects = models.ManyToManyField(PlaylistItem)
+    sound_effects = models.ForeignKey(PlaylistItem, on_delete=models.SET_DEFAULT, default=None, null=True, blank=True)
 
     def __str__(self):
         return self.name

ed818509ffa8e79ae54bc4871a8c1e37d0456f6e
Alexander Schneider <alexander@schneider.gg>
2019-03-12 13:58:18 +0100

    Add more optional fields for location

--- locations/models.py
+++ locations/models.py
@@ -1,12 +1,23 @@
+import uuid
 from django.db import models
 
 from user_manager.models import User
+
+
+def map_filepath(instance, filename):
+    random_uuid = uuid.uuid4()
+    return 'user_{0}/{1}/{2}'.format(instance.owner.id, random_uuid, filename)
 
 
 class Location(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
     name = models.CharField("Name", max_length=200, null=False, default="A wonderful place")
     description = models.TextField("Description", max_length=20000, null=True, blank=True)
+    type = models.CharField("Type", max_length=100, null=True, blank=True)
+    # TODO ruler should probably change to character-foreign key once its implemented
+    ruler = models.CharField("Ruler / Owner", max_length=100, null=True, blank=True)
+    history = models.TextField("History", max_length=20000, null=True, blank=True)
+    map = models.ImageField("Map", null=True, blank=True, upload_to=map_filepath)
 
     def __str__(self):
         return self.name

22d81fe90d78db26d88d6893289a2be7242cd0a6
Raphael Bialon <bialon@cs.uni-duesseldorf.de>
2019-04-26 16:07:56 +0200

    Add name_generator to urls

--- characters/urls.py
+++ characters/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import include, path
 
 from . import views
 
 app_name = 'characters'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('name-generator', include('characters.name_generator.urls', namespace='name-generator'))
 ]

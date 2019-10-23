cc6d8dca37a7dbc8f09eb3f86f65d8fc37c1f9e7
Alexander Schneider <alexander@schneider.gg>
2019-10-23 16:36:52 +0200

    Add models for encounters

--- encounters/urls.py
+++ encounters/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 app_name = 'encounters'
 urlpatterns = [
+    path('create/', views.CreateEncounter.as_view(), name='create'),
     path('npcs/5e/', views.IndexDnD5eNPC.as_view(), name='npc.5e'),
     path('npcs/5e/<int:npc_id>', views.DetailDnD5eNPC.as_view(), name='npc.5e.detail'),
     path('npcs/add/5e/', views.Create5eNPC.as_view(), name='npc.5e.add'),

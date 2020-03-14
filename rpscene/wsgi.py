824ad52050d9a6360b744db9baa06a8cae03a97d
Raphael Bialon <raphael.bialon@uni-duesseldorf.de>
2020-03-14 14:45:38 +0100

    Add initialization file creation for heroku nginx

--- rpscene/wsgi.py
+++ rpscene/wsgi.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 
 import os
 
+from pathlib import Path
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rpscene.settings')
 
 application = get_wsgi_application()
+Path('/tmp/app-initialized').touch()
